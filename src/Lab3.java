0d2c53190ed2d51b528474ff66188630114ec2ba
kaishoji <kaishoji.oh@gmail.com>
2021-09-09 21:51:38 -0700

    finalized lab, complete with class header and comments detailing steps taken.

--- src/Lab3.java
+++ src/Lab3.java
@@ -10,70 +10,72 @@
 import java.util.Scanner;
 
 public class Lab3 {
+
     public static void main(String[] args) throws Exception {
 
         Scanner scan = new Scanner(System.in);
 
         int i = 0;
 
-        double grade_HW = 0;
-        double grade_Midterm = 0;
-        double grade_Final = 0;
+        double gradeHW = 0;
+        double gradeMidterm = 0;
+        double gradeFinal = 0;
 
-             while (i < 3) 
-             {
-        
-                if (i == 0)
-                {
-                    System.out.println("Please enter your homework grade:");
-                    grade_HW = scan.nextDouble();
-
-                    if (grade_HW < 0 || grade_HW > 100)
-                    {
+            // this sets the condition for the while loop (while i < 3, ...)
+             while (i < 3) {
+                
+                // collects homework value
+                if (i == 0) {
+                    System.out.print("Please enter your HOMEWORK grade: ");
+                    gradeHW = scan.nextDouble();
+    
+                    // this is an input validation check, either a value < 0 or > 100 will prompt the user
+                    // for another, valid input.
+                    if (gradeHW < 0 || gradeHW > 100) {
                         System.out.println("[ERR] Invalid input. Enter a value between [0 and 100].");
-                    } else
-                        {
-                            i++;
-                        }
+                    } else {
+                        i++;
+                    }
                 }
-                if (i == 1)
-                {
-                    System.out.println("Please enter your midterm grade:");
-                    grade_Midterm = scan.nextDouble();
 
-                    if (grade_Midterm < 0 || grade_Midterm > 100)
-                    {
+                // collects midterm value
+                if (i == 1) {
+                    System.out.print("Please enter your MIDTERM grade: ");
+                    gradeMidterm = scan.nextDouble();
+
+                    // this is another input validation check
+                    if (gradeMidterm < 0 || gradeMidterm > 100) {
                         System.out.println("[ERR] Invalid input. Enter a value between [0 and 100].");
-                    } else
-                        {
-                            i++;
-                        }
-                if (i == 2)
-                {
-                    System.out.println("Please enter your final exam grade:");
-                    grade_Final = scan.nextDouble();
+                    } else {
+                        i++;
+                    }
+                }
 
-                    if (grade_Final < 0 || grade_Final > 200)
-                    {
+                // collects the final exam value
+                if (i == 2) {
+                    System.out.print("Please enter your FINAL exam grade: ");
+                    gradeFinal = scan.nextDouble();
+
+                    // this is another input validation check
+                    if (gradeFinal < 0 || gradeFinal > 200) {
                         System.out.println("[ERR] Invalid input. Enter a value between [0 and 200].");
-                    } else 
-                        {
-                            i++;
-                        }
+                    } else {
+                        i++;
+                    }
                 }
             } 
-        }
-        double total_weighted;
+        double totalWeighted; 
 
-        total_weighted = (grade_Final / 200 * 50) + (grade_Midterm * 0.25) + (grade_HW * 0.25);
+        // calculates the total weighted final grade, and stores it in the variable
+        totalWeighted = (gradeFinal / 200 * 50) + (gradeMidterm * 0.25) + (gradeHW * 0.25);
 
-        if (total_weighted >= 50) 
-        {
-            System.out.println("[INFO] Student's Weighted Total is " + total_weighted);
-            System.out.println("[INFO] Student PASSED the class.");
-        }   else
-            {
-                System.out.println("[INFO] Student's Weighted Total is " + total_weighted);
+            // checks if the final weighted grade allows a student to pass...
+            if (totalWeighted >= 50) {
+                System.out.println("[INFO] Student's Weighted Total is " + totalWeighted);
+                System.out.println("[INFO] Student PASSED the class.");
+                // checks if the final weighted grade allows a student to pass...
+            }  else {
+                System.out.println("[INFO] Student's Weighted Total is " + totalWeighted);
                 System.out.println("[INFO] Student FAILED the class.");
             }
     }
